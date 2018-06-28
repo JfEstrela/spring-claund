e0e544339d9d03fe62e0cd55e3656b9840ca31aa
JfEstrela <jfernandoestrela88@gmail.com0>
2018-06-27 23:50:36 -0300

    first commit

---
+++ lab01/spring-cloud-client-server/src/main/java/com/jf/estrela/microserviceclass/cloud/client/SpringCloudClientServerApplication.java
@@ -0,0 +1,12 @@
+package com.jf.estrela.microserviceclass.cloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClientServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClientServerApplication.class, args);
+	}
+}
