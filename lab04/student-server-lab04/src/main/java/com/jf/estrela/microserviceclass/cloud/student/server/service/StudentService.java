e007a36d8ba65631b8c30f4ddfdc8d861be15edc
JfEstrela <jfernandoestrela88@gmail.com0>
2018-07-02 20:52:01 -0300

    commit lab04

---
+++ lab04/student-server-lab04/src/main/java/com/jf/estrela/microserviceclass/cloud/student/server/service/StudentService.java
@@ -0,0 +1,27 @@
+package com.jf.estrela.microserviceclass.cloud.student.server.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jf.estrela.microserviceclass.cloud.student.server.dto.StudentDTO;
+import com.jf.estrela.microserviceclass.cloud.student.server.feign.DisciplineClient;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	DisciplineClient disciplineClient;
+	 
+	 
+	 @SuppressWarnings("unchecked")
+	public StudentDTO getStudentesAndDisciplines() {
+		 StudentDTO dto = new StudentDTO();
+		 dto.setName("Teste");
+		 dto.setId(1L);
+		 dto.setRegistration(1);
+		 dto.setEmail("teste@teste");
+		 dto.setDisciplines(disciplineClient.getAllDisciplinas().getBody());
+		 return dto;
+	 }
+
+}
